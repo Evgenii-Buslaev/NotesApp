f7fc40e73aecdf5042893f46060a8cc4015df539
Evgenii Buslaev <eugenebus144@gmail.com>
2022-08-12 15:11:33 +0500

    Created a movement handler for Note methods

--- src/scripts/Note.js
+++ src/scripts/Note.js
@@ -1,16 +1,16 @@
-import { renderSavedItems } from "./handlers.js";
-
 export default class Note {
   static _notesCollection = [];
   static _notificationCollection = [];
   static _removedCollection = [];
   static changedState = Note._notesCollection;
 
-  constructor(value, noteElement) {
+  constructor(value, noteElement, _movementHandler) {
     // note-object properties
     this.value = value;
     this._note = noteElement;
     this.id = Math.random();
+    // movment handler
+    this.movement_handler = _movementHandler;
   }
 
   generateNote() {
@@ -50,46 +50,18 @@ export default class Note {
   }
 
   moveToNotification() {
-    Note._notificationCollection.push(this);
-    for (let i = 0; i < Note._notesCollection.length; i++) {
-      if (this.id === Note._notesCollection[i].id) {
-        Note._notesCollection[i].element.style.opacity = "0";
-        Note._notesCollection.splice(i, 1);
-        Note.changedState = Note._notesCollection;
-      }
-    }
-    for (let i = 0; i < Note._removedCollection.length; i++) {
-      if (this.id === Note._removedCollection[i].id) {
-        Note._removedCollection[i].element.style.opacity = "0";
-        Note._removedCollection.splice(i, 1);
-        Note.changedState = Note._removedCollection;
-      }
-    }
-    setTimeout(() => {
-      console.log(Note.changedState);
-      renderSavedItems(Note.changedState);
-    }, 500);
+    this.movement_handler(
+      Note._notesCollection,
+      Note._removedCollection,
+      Note._notificationCollection
+    );
   }
 
   removeNote() {
-    Note._removedCollection.push(this);
-    for (let i = 0; i < Note._notesCollection.length; i++) {
-      if (this.id === Note._notesCollection[i].id) {
-        Note._notesCollection[i].element.style.opacity = "0";
-        Note._notesCollection.splice(i, 1);
-        Note.changedState = Note._notesCollection;
-      }
-    }
-    for (let i = 0; i < Note._notificationCollection.length; i++) {
-      if (this.id === Note._notificationCollection[i].id) {
-        Note._notificationCollection[i].element.style.opacity = "0";
-        Note._notificationCollection.splice(i, 1);
-        Note.changedState = Note._notificationCollection;
-      }
-    }
-    setTimeout(() => {
-      console.log(Note.changedState);
-      renderSavedItems(Note.changedState);
-    }, 500);
+    this.movement_handler(
+      Note._notesCollection,
+      Note._notificationCollection,
+      Note._removedCollection
+    );
   }
 }
--- src/scripts/handlers.js
+++ src/scripts/handlers.js
@@ -1,5 +1,6 @@
 import Note from "../scripts/Note.js";
 
+// input part
 export function addNote(className) {
   if (document.getElementById("input-text").value === "") {
     alert("Заметка не может быть пустой");
@@ -7,7 +8,8 @@ export function addNote(className) {
   }
   let note = new className(
     document.getElementById("input-text").value,
-    document.querySelector(".note")
+    document.querySelector(".note"),
+    moveItemsBetweenSections
   );
   note.generateNote();
   note.renderNote(document.querySelector(".notes"));
@@ -36,6 +38,8 @@ export function removeInput() {
   document.getElementById("input-text").value = "";
 }
 
+// items part
+
 export function renderSavedItems(itemsArray) {
   if (itemsArray !== Note._notesCollection) {
     document.getElementById("note-container").style.opacity = "0";
@@ -48,6 +52,31 @@ export function renderSavedItems(itemsArray) {
     itemsArray[i]._setHandlers();
   }
   console.log(document.querySelector(".notes").children);
+}
+
+export function moveItemsBetweenSections(
+  fromSectionOne,
+  fromSectionTwo,
+  targetSection
+) {
+  targetSection.push(this);
+  for (let i = 0; i < fromSectionOne.length; i++) {
+    if (this.id === fromSectionOne[i].id) {
+      fromSectionOne[i].element.style.opacity = "0";
+      fromSectionOne.splice(i, 1);
+      Note.changedState = fromSectionOne;
+    }
+  }
+  for (let i = 0; i < fromSectionTwo.length; i++) {
+    if (this.id === fromSectionTwo[i].id) {
+      fromSectionTwo[i].element.style.opacity = "0";
+      fromSectionTwo.splice(i, 1);
+      Note.changedState = fromSectionTwo;
+    }
+  }
+  setTimeout(() => {
+    renderSavedItems(Note.changedState);
+  }, 500);
 }
 
 export function pinHandler(note) {
