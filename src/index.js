5bbeb8f416a2cd5b4ff6eae3a494d1f2460c183d
Evgenii Buslaev <eugenebus144@gmail.com>
2022-08-15 16:48:47 +0500

    Added render of items from localStorage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Note from "./scripts/Classes/Note.js";
 
+import { moveItemsBetweenSections } from "./scripts/handlers/sections.js";
 // singletons
 import { sections, mode } from "./scripts/singletons.js";
 
@@ -11,16 +12,49 @@ sections.renderStorageSection();
 sections._setStorageHandlers();
 
 // working with localStorage
-localStorage.clear();
+/* localStorage.clear(); */
 
 window.addEventListener("load", () => {
+  // creating note items from localStorage
   if (localStorage.getItem("itemsArray")) {
     Note._notesCollection = JSON.parse(localStorage.getItem("itemsArray"))[0];
+    for (let i = 0; i < Note._notesCollection.length; i++) {
+      let note = new Note(
+        Note._notesCollection[i].value,
+        document.querySelector(".note"),
+        moveItemsBetweenSections
+      );
+      Note._notesCollection.splice(i, 1, note);
+      note.generateNote();
+      note.renderNote(document.querySelector(".notes"));
+      note._setHandlers();
+    }
+
     Note._folderCollection = JSON.parse(localStorage.getItem("itemsArray"))[1];
+    for (let i = 0; i < Note._folderCollection.length; i++) {
+      let note = new Note(
+        Note._folderCollection[i].value,
+        document.querySelector(".note"),
+        moveItemsBetweenSections
+      );
+      Note._folderCollection.splice(i, 1, note);
+      note.generateNote();
+      note.renderNote(document.querySelector(".notes"));
+      note._setHandlers();
+    }
+
     Note._removedCollection = JSON.parse(localStorage.getItem("itemsArray"))[2];
-    console.log(Note._notesCollection);
-    console.log(Note._folderCollection);
-    console.log(Note._removedCollection);
+    for (let i = 0; i < Note._removedCollection.length; i++) {
+      let note = new Note(
+        Note._removedCollection[i].value,
+        document.querySelector(".note"),
+        moveItemsBetweenSections
+      );
+      Note._removedCollection.splice(i, 1, note);
+      note.generateNote();
+      note.renderNote(document.querySelector(".notes"));
+      note._setHandlers();
+    }
 
     sections.renderStorageSection();
     sections._setStorageHandlers();
